d379c4405ebcc620c8139d73df1d741cd4a09f70
slava1688 <slava1688@yandex.ru>
2022-03-17 01:13:04 +0300

    ENH: pipeline: training and testing with render

--- python/pipeline.py
+++ python/pipeline.py
@@ -2,67 +2,107 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
 from itertools import chain
 
-from nets import Synthesizer, Recognizer
+from nets import Synthesizer, Recognizer, Renderer
 from test import test
 from train import train
 
-bit_string_length = 50
-marker_size = 32
+from utils import COCOval
+
+
+n = 32
+m = 32
+
+background_size = m * 2
+resize_source_back = background_size * 2
 
 batch_size = 16
-num_strings = 1000
-
+#num_strings = 1000
+epochs = 1
 lr = 1e-4
 weight_decay = 1e-5
-epochs = 400
 
 synt_net_name = '../trained_nets/synt_net.pth'
+rend_net_name = '../trained_nets/rend_net.pth'
 rec_net_name = '../trained_nets/rec_net.pth'
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def training_pipeline(save=True):
-        synt_net = Synthesizer(bit_string_length, marker_size)
-        rec_net = Recognizer(bit_string_length)
+	background_train = COCOval(resize_source_back,
+               root_dir='../background/train', 
+               transform = transforms.Compose([
+                   transforms.ToTensor(),
+                   transforms.RandomCrop(background_size)
+               ]))
 
-        synt_net.to(device)
-        rec_net.to(device)
+	background_dataloader_train = torch.utils.data.DataLoader(background_train, 
+                                                    batch_size=batch_size, 
+                                                    shuffle=True, 
+                                                    num_workers=2)
+                                                    
+	synt_net = Synthesizer(n, m)
+	rend_net = Renderer(background_size)
+	rec_net = Recognizer(n)
 	
-        criterion = nn.Sigmoid()
-
-        params = chain(synt_net.parameters(), rec_net.parameters())
-        optimizer = optim.Adam(params, lr=lr, weight_decay=weight_decay)
-
-        print('Start training')
-        train(device, num_strings, batch_size, 
-                synt_net, rec_net, 
-		criterion, optimizer, epochs, bit_string_length)
-        print('Finish training')
-
-        if save:
+	synt_net.to(device)
+	rend_net.to(device)
+	rec_net.to(device)
+	
+	criterion = nn.Sigmoid()
+	
+	params = chain(synt_net.parameters(), rend_net.parameters(), rec_net.parameters())
+	optimizer = optim.Adam(params, lr=lr, weight_decay=weight_decay)
+	
+	print('Start training')
+	train(device, background_dataloader_train, 
+                synt_net, rend_net, rec_net, 
+                criterion, optimizer, epochs, n, m)
+	print('Finish training')
+        
+	if save:
                 torch.save(synt_net.state_dict(), synt_net_name)
+                torch.save(rend_net.state_dict(), rend_net_name)
                 torch.save(rec_net.state_dict(), rec_net_name)
 
 
 def testing_pipeline():
-        synt_net = Synthesizer(bit_string_length, marker_size)
-        rec_net = Recognizer(bit_string_length)
+	background_test = COCOval(resize_source_back,
+               root_dir='../background/val', 
+               transform = transforms.Compose([
+                   transforms.ToTensor(),
+                   transforms.Resize(background_size)
+               ]))
 
-        synt_net.load_state_dict(torch.load(synt_net_name))
-        rec_net.load_state_dict(torch.load(rec_net_name))
-	
-        synt_net.to(device)
-        rec_net.to(device)
-	
-        synt_net.eval()
-        rec_net.eval()
+	background_dataloader_test = torch.utils.data.DataLoader(background_test, 
+                                                    batch_size=batch_size, 
+                                                    shuffle=True, 
+                                                    num_workers=2)
 
-        print('Start testing')
-        test(device, num_strings, batch_size, 
-		synt_net, rec_net, bit_string_length)
+	synt_net = Synthesizer(n, m)
+	rend_net = Renderer(background_size)
+	rec_net = Recognizer(n)
+
+	synt_net.load_state_dict(torch.load(synt_net_name))
+	rend_net.load_state_dict(torch.load(rend_net_name))
+	rec_net.load_state_dict(torch.load(rec_net_name))
+	
+	synt_net.to(device)
+	rend_net.to(device)
+	rec_net.to(device)
+        
+	synt_net.eval()
+	rend_net.eval()
+	rec_net.eval()
+
+	print('Start testing')
+	test(device, background_dataloader_test, 
+                synt_net, rend_net, rec_net, n, m)
 
 if __name__ == '__main__':
         training_pipeline()
