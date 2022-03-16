7db7ed2bb874750626e8d6b0578b2daaa800ca8f
slava1688 <slava1688@yandex.ru>
2022-03-16 23:57:28 +0300

    ENH: nets: add render net

--- python/nets.py
+++ python/nets.py
@@ -1,7 +1,19 @@
 import torch
 import torch.nn as nn
 
+import numpy as np
+import kornia as K
+
 from utils import Reshape
+
+
+class Reshape(nn.Module):
+    def __init__(self, *args):
+        super(Reshape, self).__init__()
+        self.shape = args
+
+    def forward(self, x):
+        return x.view(self.shape)
 
 
 class Synthesizer(nn.Module):
@@ -49,7 +61,8 @@ class Recognizer(nn.Module):
         self.pool = nn.MaxPool2d(3, padding=(1, 0))
         self.relu = nn.ReLU()
         self.batchnorm = nn.BatchNorm2d(96)
-        self.fc = nn.Linear(192, n)
+        #self.fc = nn.Linear(192, n) # w/o background
+        self.fc = nn.Linear(576, n) # with background
         
     def forward(self, x):
         x = x.float()
@@ -65,4 +78,36 @@ class Recognizer(nn.Module):
         x = self.fc(x)
         
         return x.double()
+    
+    
+class Renderer(nn.Module):
+    def __init__(self, size):
+        super(Renderer, self).__init__()        
+        self.color_jitter = K.augmentation.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5)
+        self.size = size # size of marker (with background)
+  
+    def forward(self, x):
+        # affine transformation
+        affine_sigma = np.random.uniform(0.01, 0.1, 1)
+        affine_matrix = torch.tensor([[1, 0, 0], [0, 1, 0]]) +\
+            torch.normal(torch.zeros(2, 3), torch.full((2, 3), float(affine_sigma)))
+        x = K.geometry.transform.affine(x.float(), affine_matrix)
+        
+        # noise
+        for i in range(x.shape[0]):
+            # same parameters for each channel
+            g = float(np.random.uniform(0.001, 0.003, 1))
+            Jahne_sigma = float(np.random.uniform(0.001, 0.3, 1))
+            std = torch.sqrt(g * torch.mean(x[i], axis=[1, 2]) + Jahne_sigma ** 2)
+            for j in range(x[i].shape[0]):
+                x[i][j] = x[i][j] +\
+                    torch.normal(torch.zeros(self.size, self.size), torch.full((self.size, self.size), float(std[j])))
+        
+        # color transformation
+        x = self.color_jitter(x)
+        
+        # blurring
+        blur_sigma = np.random.uniform(0.001, 0.5, 1)
+        x = K.filters.gaussian_blur2d(x, (3, 3), (blur_sigma, blur_sigma))
+        return x
 
